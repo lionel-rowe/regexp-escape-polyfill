df25d3255fdee097fa21449151d98a7555fdd623
lionel-rowe <lionel.rowe@gmail.com>
2024-08-22 23:30:05 +0800

    Clobber instead of conditional load in polyfill version

--- polyfill.mjs
+++ polyfill.mjs
@@ -3,12 +3,9 @@
 import { regExpEscape } from './escape.mjs'
 
 /** @type {import('./types.ts')} */
-
-if (!Object.hasOwn(RegExp, 'escape')) {
-	Object.defineProperty(RegExp, 'escape', {
-		value: regExpEscape,
-		writable: true,
-		enumerable: false,
-		configurable: true,
-	})
-}
+Object.defineProperty(RegExp, 'escape', {
+	value: regExpEscape,
+	writable: true,
+	enumerable: false,
+	configurable: true,
+})
