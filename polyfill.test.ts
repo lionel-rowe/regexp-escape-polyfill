09f7f0bad5669c567a98c5f9727b8a22e62a5675
lionel-rowe <lionel.rowe@gmail.com>
2024-08-23 16:18:33 +0800

    Run each test262 test in its own process

--- polyfill.test.ts
+++ polyfill.test.ts
@@ -1,40 +1,40 @@
 #!/usr/bin/env -S deno test -A --watch
 
-import './polyfill.mjs'
 import { walk } from '@std/fs'
 
-for (
-	const [fileName, idents] of [
-		['assert.js', ['assert']],
-		['isConstructor.js', ['isConstructor']],
-		['sta.js', ['Test262Error']],
-		['propertyHelper.js', ['verifyProperty']],
-	] as const
-) {
-	eval(
-		`${await Deno.readTextFile(`./test262/harness/${fileName}`)}\n${
+async function getCode(testPath: string) {
+	let code = ''
+
+	for (
+		const [fileName, idents] of [
+			['assert.js', ['assert']],
+			['isConstructor.js', ['isConstructor']],
+			['sta.js', ['Test262Error']],
+			['propertyHelper.js', ['verifyProperty']],
+		] as const
+	) {
+		code += `${await Deno.readTextFile(`./test262/harness/${fileName}`)}\n${
 			idents.map((ident) => `globalThis.${ident} = ${ident}`).join('\n')
-		}`,
-	)
+		}\n`
+	}
+
+	code += `await import('./polyfill.mjs')\n`
+	code += `await import('./${testPath}')\n`
+
+	return code
 }
 
-const ignoredTests = [
-	// skip
-	'cross-realm.js',
-	// run after all the other tests as it deletes the RegExp.escape property as a side-effect
-	'prop-desc.js',
-]
+const ignoredTests = ['cross-realm.js']
 
 Deno.test('RegExp.escape', async (t) => {
 	for await (const entry of walk('./test262/test/built-ins/RegExp/escape/')) {
 		if (entry.isFile && !ignoredTests.includes(entry.name)) {
 			await t.step(entry.name, async () => {
-				await import(import.meta.resolve('./' + entry.path))
+				await new Deno.Command(Deno.execPath(), {
+					args: ['eval', await getCode(entry.path)],
+					cwd: Deno.cwd(),
+				}).spawn().output()
 			})
 		}
 	}
-
-	await t.step('prop-desc.js', async () => {
-		await import(import.meta.resolve('./test262/test/built-ins/RegExp/escape/prop-desc.js'))
-	})
 })
